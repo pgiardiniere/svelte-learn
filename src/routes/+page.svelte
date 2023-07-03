56a4869a2f2b16da83de6478b8b0d3fcb542bf08
Peter Giardiniere <pgiardiniere@gmail.com>
2023-07-03 18:48:15 -0400

    Complete p3 routing pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<script>
-  let src = '/image.gif'
-</script>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
 
-<img {src} alt="a man dances."/>
+<h1>home</h1>
+<p>this is the home page.</p>
